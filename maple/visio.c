IepIweidieng/dreambbs

commit eb14b9560bfbd60d0e1acb26575bd097858ab1bb

maple/ & lib/: Signal handler functions: Avoid casting them into incompatible function pointer type.

maple/visio.c
@@ -1631,7 +1631,8 @@ igetch(void)
              * Thor.980307: 想不到什麼好方法, 在^R時禁止talk, 否則會因,
              * 沒有vio_fd, 看不到 I_OTHERDATA 所以在 ctrl-r時talk, 看不到對方打的字
              */
-            signal(SIGUSR1, (void (*)(int signum))talk_rqst);
+            extern void (*talk_rqst_signal)(int signum);
+            signal(SIGUSR1, talk_rqst_signal);
 
             continue;
         }

lib/dns.c
@@ -163,8 +163,9 @@ unsigned long dns_addr(char *name)
 #ifdef RFC931_TIMEOUT
 static const int timeout = 1;    /* 若 1 秒後連線未完成，則放棄 */
 
-static void pseudo_handler(void)  /* Thor.991215: for timeout */
+static void pseudo_handler(int signum)  /* Thor.991215: for timeout */
 {
+    (void)signum;
     /* connect time out */
 }
 #endif

maple/bbsd.c
@@ -1037,6 +1037,21 @@ tn_login(void)
 /* trap signals                                          */
 /* ----------------------------------------------------- */
 
+static void abort_bbs_signal(int signum)
+{
+    (void)signum;
+    abort_bbs();
+}
+void talk_rqst_signal(int signum)
+{
+    (void)signum;
+    talk_rqst();
+}
+static void bmw_rqst_signal(int signum)
+{
+    (void)signum;
+    bmw_rqst();
+}
 
 static void
 tn_signals(void)
@@ -1047,7 +1062,7 @@ tn_signals(void)
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
 
-    act.sa_handler = (void (*)(int signum))abort_bbs;
+    act.sa_handler = abort_bbs_signal;
     sigaction(SIGBUS, &act, NULL);
     sigaction(SIGSEGV, &act, NULL);
     sigaction(SIGTERM, &act, NULL);
@@ -1057,10 +1072,10 @@ tn_signals(void)
     sigaction(SIGSYS, &act, NULL);/* bad argument to system call */
 #endif
 
-    act.sa_handler = (void (*)(int signum))talk_rqst;
+    act.sa_handler = talk_rqst_signal;
     sigaction(SIGUSR1, &act, NULL);
 
-    act.sa_handler = (void (*)(int signum))bmw_rqst;
+    act.sa_handler = bmw_rqst_signal;
     sigaction(SIGUSR2, &act, NULL);
 
     /* sigblock(sigmask(SIGPIPE)); */

commit 79eb990e8b8527b5dea21884a97c6acc1ffe8416

maple/visio.c: `igetch()`: Fix wrongly declaring function `talk_rqst_signal()` as a pointer, introduced in commit eb14b9560bfbd60d0e1acb26575bd097858ab1bb.

maple/visio.c
@@ -1655,7 +1655,7 @@ igetch(void)
              * Thor.980307: 想不到什麼好方法, 在^R時禁止talk, 否則會因,
              * 沒有vio_fd, 看不到 I_OTHERDATA 所以在 ctrl-r時talk, 看不到對方打的字
              */
-            extern void (*talk_rqst_signal)(int signum);
+            extern void talk_rqst_signal(int signum);
             signal(SIGUSR1, talk_rqst_signal);
 
             continue;