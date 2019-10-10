IepIweidieng/dreambbs

commit 38c3c01232705ac9eec032d01b3786609b73c4e9

maple/board.c: add workaround bug fix for empty class_hot
before this commit, empty class_hot may cause abort_bbs problem for BBS users

thanks to ideas from related source code in MapleBridge BBS ( telnet://imaple.tw )

maple/board.c
@@ -1034,7 +1034,7 @@ class_load(
       // 即時熱門看板臨界值自定
       if (class_hot)
       {
-        if (bshm->mantime[chn] < CLASS_HOT) /* 只列出人氣超過 CLASS_HOT 的看板 */
+        if ( ( bshm->mantime[chn] < CLASS_HOT ) && ( strcmp(brd[chn].brdname, "SYSOP") ) ) /* 只列出人氣超過 CLASS_HOT 的看板 */
           continue;
         bnum++;
       }
@@ -1048,7 +1048,7 @@ class_load(
     *cbase++ = chn;
   } while (chead < ctail);
 
-  if (bnum > 0)
+  if (class_hot && bnum > 0)
     qsort(cbase - bnum, bnum, sizeof(short), mantime_cmp);
 
   xo->max = max;

commit 54de16bd832b485f600780f846565f82373b3038

maple/board.c: `class_load()`: Fix the hottest board list only listing the board 'SYSOP', caused by 38c3c01232705ac9eec032d01b3786609b73c4e9. [ref: #47]
The hottest board list now lists all boards
   except the board 'SYSOP'.

maple/board.c
@@ -1040,7 +1040,7 @@ class_load(
             // 即時熱門看板臨界值自定
             if (class_hot)
             {
-                if ( ( bshm->mantime[chn] < CLASS_HOT ) && ( strcmp(brd[chn].brdname, "SYSOP") ) ) /* 只列出人氣超過 CLASS_HOT 的看板 */
+                if ( ( bshm->mantime[chn] < CLASS_HOT ) && ( !strcmp(brd[chn].brdname, "SYSOP") ) ) /* 只列出人氣超過 CLASS_HOT 的看板 */
                     continue;
                 bnum++;
             }