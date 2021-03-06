IepIweidieng/dreambbs

commit 1484336c0cecc57beea583ccb839e18c7c6b2ae3

`bbsd` & `xchatd`: Enable ident and dns if macro `NOIDENT` is undefined.
- maple/bbsd: `start_daemon()` & `servo_daemon()`:
   Call `dns_init()` before `fork()` if `NOIDENT` is undefined
- maple/bbsd:
   `main()`: Enable ident if `NOIDENT` is undefined.
   Add comment about setting `mport` if connection data are passed
   Clarify the first argument to `dns_ident()`
- maple/xchatd.c: `login_user()`: Enable dns if `NOIDENT` is undefined.

maple/bbsd.c
@@ -1399,6 +1399,9 @@ start_daemon(
         time(&val);
         strftime(buf, 80, "%d/%b/%Y %H:%M:%S", localtime(&val));
     }
+#ifndef NOIDENT
+    dns_init();
+#endif
 
 #ifdef RLIMIT
     /* --------------------------------------------------- */
@@ -1804,6 +1807,7 @@ int main(int argc, char *argv[])
                 break;
             default:;
             }
+            /* mport = cdata.lport; */
         }
 
         switch (sin.ss_family)
@@ -1854,11 +1858,14 @@ int main(int argc, char *argv[])
         /* rfc931((struct sockaddr *)&sin, fromhost, rusername); */
 
         tn_addr = *(ip_addr *)&sin;
-        /* Thor.990325: 修改dns_ident定義, 來自哪if連那 */
-        /* dns_ident(mport, &tn_addr, fromhost, sizeof(fromhost), rusername, sizeof(rusername)); */
 
+#ifdef NOIDENT
         /* cache.090728: 連線不反查, 增加速度 */
-        getnameinfo((struct sockaddr *)&tn_addr, sizeof(tn_addr), fromhost, sizeof(fromhost), NULL, NI_MAXSERV, NI_NUMERICHOST);
+        getnameinfo((struct sockaddr *)&tn_addr, sizeof(tn_addr), fromhost, sizeof(fromhost), NULL, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+#else
+        /* Thor.990325: 修改dns_ident定義, 來自哪if連那 */
+        dns_ident(0 /* listen fd */, &tn_addr, fromhost, sizeof(fromhost), rusername, sizeof(rusername));
+#endif
 
         telnet_init();
         term_init();

maple/xchatd.c
@@ -1970,12 +1970,14 @@ login_user(
 
     /* Xshadow: 取得 client 的來源 */
 
+#ifdef NOIDENT
     getnameinfo((struct sockaddr *)cu->rhost, sizeof(cu->rhost), cu->rhost, sizeof(cu->rhost), NULL, NI_MAXSERV, NI_NUMERICHOST);
-
-    /* dns_name((ip_addr *)cu->rhost, cu->ibuf, sizeof(cu->ibuf)); */
-    /* str_ncpy(cu->rhost, cu->ibuf, sizeof(cu->rhost)); */
-#if 0
+#else
+    dns_name((ip_addr *)cu->rhost, cu->ibuf, sizeof(cu->ibuf));
+    str_ncpy(cu->rhost, cu->ibuf, sizeof(cu->rhost));
+  #if 0
     getnameinfo((struct sockaddr *)cu->rhost, sizeof(cu->rhost), cu->rhost, sizeof(cu->rhost), NULL, NI_MAXSERV, 0);
+  #endif
 #endif
 
     cu->userno = utent;
@@ -3017,6 +3019,10 @@ servo_daemon(
     time((time_t *) &value);
     gmtime((time_t *) &value);
     strftime(buf, 80, "%d/%b/%Y:%H:%M:%S", localtime((time_t *) &value));
+
+#ifndef NOIDENT
+    dns_init();
+#endif
 
 #ifdef RLIMIT
     /* --------------------------------------------------- */

commit f2704b762ec8a9d24d2b885419faef8e4e6e79cd

docs(bbsd main()): fix wrong comment for 1st arg to dns_ident() introduced in 1484336c0cecc57beea583ccb839e18c7c6b2ae3
`mport` represents the port the client connect to.
It has nothing to do with file descriptors.

maple/bbsd.c
@@ -1874,7 +1874,7 @@ int main(int argc, char *argv[])
         getnameinfo((struct sockaddr *)&tn_addr, sizeof(tn_addr), fromhost, sizeof(fromhost), NULL, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
 #else
         /* Thor.990325: 修改dns_ident定義, 來自哪if連那 */
-        dns_ident(0 /* listen fd */, &tn_addr, fromhost, sizeof(fromhost), rusername, sizeof(rusername));
+        dns_ident(0 /* mport */, &tn_addr, fromhost, sizeof(fromhost), rusername, sizeof(rusername));
 #endif
 
         telnet_init();