IepIweidieng/dreambbs

commit d0f246b0247bda81f66f20fdd928c24bbc32b4b5

maple/popupmenu.c & maple/xover.c: `Every_Z_Screen()`: Support screenshot with pfterm.

maple/popupmenu.c
@@ -652,7 +652,6 @@ int pmsg(char *msg)
     return cc;
 }
 
-#ifndef M3_USE_PFTERM
 /* verit. 030211 抓取螢幕畫面到暫存檔 */
 int
 Every_Z_Screen(void)
@@ -660,6 +659,11 @@ Every_Z_Screen(void)
     FILE *fp;
     int i;
     char buf[512];
+#ifdef M3_USE_PFTERM
+    int oy, ox;
+
+    getyx(&oy, &ox);
+#endif
 
     fp = tbf_open();
     if (!fp)
@@ -670,11 +674,19 @@ Every_Z_Screen(void)
     for (i=0; i<=b_lines; ++i)
     {
         memset(buf, 0, sizeof(buf));
+#ifdef M3_USE_PFTERM
+        move(i, 0);
+        inansistr(buf, 512);
+#else
         strncpy(buf, (char *) sl[i].data, sl[i].len);
+#endif
         fprintf(fp, "%s\n", buf);
     }
     fclose(fp);
+
+#ifdef M3_USE_PFTERM
+    move(oy, ox);
+#endif
     return 1;
 }
-#endif
 

maple/xover.c
@@ -2113,9 +2113,7 @@ Every_Z_BMW(void)
     return 0;
 }
 
-#ifndef M3_USE_PFTERM
 extern int Every_Z_Screen(void);
-#endif
 
 static MENU menu_everyz[] =
 {
@@ -2142,10 +2140,8 @@ static MENU menu_everyz[] =
     {Every_Z_BMW, 0, POPUP_FUN,
     "Bmw      熱訊紀錄"},
 
-#ifndef M3_USE_PFTERM
     {Every_Z_Screen, 0, POPUP_FUN,
     "Screen   螢幕擷取"},
-#endif
 
     {NULL, 0, POPUP_QUIT,
     "Quit     離開"},

commit c3364d20b39b0165e070f163d5c2391967f27af5

fix(Every_Z_Screen()): fix picture of Ctrl-Z menu occurs in screenshot
Fix pfterm screenshot broken since d0f246b0247bda81f66f20fdd928c24bbc32b4b5.
Fix native visio screenshot broken since b7bb1371d7c67b74a6c533e1f5be8962e3032d48.

Now Every_Z_Screen() will try to use the screen saved by popupmenu()
before the use of current screen which may include the picture of Ctrl-Z menu.

* maple/popupmenu.c:
    * popupmenu():
        * Make old_screen global and rename it to popup_old_screen
    * Every_Z_Screen():
        * Try to restore the saved screen from popup_old_screen
          before taking the screenshot
        * Fall back to use the current screen if the saved screen is not available

maple/popupmenu.c
@@ -8,6 +8,9 @@
 
 #include "bbs.h"
 
+/* For screenshot */
+static screen_backup_t *popup_old_screen = NULL;
+
 static int do_menu(MENU pmenu[], XO *xo, int y_ref, int x_ref);
 
 /* ----------------------------------------- */
@@ -362,6 +365,7 @@ do_menu(
 
 
     scr_dump(&old_screen);
+    popup_old_screen = &old_screen;
 
 do_menu_redraw:
     y = gety_ref(y_ref);
@@ -379,13 +383,20 @@ do_menu_redraw:
             if (xo)
             {
                 /* Redraw and redump */
+                popup_old_screen = NULL;
                 scr_restore_free(&old_screen);
+
                 xover_exec_cb(xo, XO_HEAD);
                 cursor_show(3 + xo->pos - xo->top, 0);
+
                 scr_dump(&old_screen);
+                popup_old_screen = &old_screen;
             }
             else
+            {
                 scr_restore_keep(&old_screen);
+                popup_old_screen = &old_screen;
+            }
             goto do_menu_redraw;
         }
 
@@ -395,6 +406,7 @@ do_menu_redraw:
             case KEY_LEFT:
             case KEY_ESC:
             case Meta(KEY_ESC):
+                popup_old_screen = NULL;
                 scr_restore_free(&old_screen);
                 return 1;
             case KEY_UP:
@@ -413,15 +425,18 @@ do_menu_redraw:
             case '\n':
                 if (table[cur]->umode & M_QUIT)
                 {
+                    popup_old_screen = NULL;
                     scr_restore_free(&old_screen);
                     return 1;
                 }
                 if (do_cmd(table[cur], xo, y, x)<0)
                 {
+                    popup_old_screen = NULL;
                     scr_restore_free(&old_screen);
                     return -1;
                 }
                 scr_restore_keep(&old_screen);
+                popup_old_screen = &old_screen;
                 draw_menu((const MENU *const *)table, num+1, title, y, x, cur);
                 break;
             default:
@@ -434,15 +449,18 @@ do_menu_redraw:
                         {
                             if (table[cur]->umode & M_QUIT)
                             {
+                                popup_old_screen = NULL;
                                 scr_restore_free(&old_screen);
                                 return 1;
                             }
                             if (do_cmd(table[cur], xo, y, x)<0)
                             {
+                                popup_old_screen = NULL;
                                 scr_restore_free(&old_screen);
                                 return -1;
                             }
                             scr_restore_keep(&old_screen);
+                            popup_old_screen = &old_screen;
                             draw_menu((const MENU *const *)table, num+1, title, y, x, cur);
                         }
                         break;
@@ -693,6 +711,9 @@ Every_Z_Screen(void)
     FILE *fp;
     int i;
     char buf[512];
+
+    screen_backup_t old_screen = {0};
+
 #ifdef M3_USE_PFTERM
     int oy, ox;
 
@@ -705,6 +726,15 @@ Every_Z_Screen(void)
         vmsg("ÀÉ®×¶}±Ò¿ù»~ !!");
         return 0;
     }
+
+    /* IID.2021-02-26: Restore the screen saved by `popupmenu()`
+     * or use the current screen if the saved screen is not available */
+    if (popup_old_screen && popup_old_screen->IF_ON(M3_USE_PFTERM, raw_memory, slp))
+    {
+        scr_dump(&old_screen);
+        scr_restore_keep(popup_old_screen);
+    }
+
     for (i=0; i<=b_lines; ++i)
     {
 #ifdef M3_USE_PFTERM
@@ -721,8 +751,12 @@ Every_Z_Screen(void)
     }
     fclose(fp);
 
+    /* Restore the screen if the saved screen is used */
+    if (old_screen.IF_ON(M3_USE_PFTERM, raw_memory, slp))
+        scr_restore_free(&old_screen);
 #ifdef M3_USE_PFTERM
-    move(oy, ox);
+    else
+        move(oy, ox);
 #endif
     return 1;
 }