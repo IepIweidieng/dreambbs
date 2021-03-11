IepIweidieng/dreambbs

commit 39902b515d8fa67d8b39e62e7ff1dd991b3b883d

Avoid unsigned division and refine unsigned modulo
- Replace unsigned division with normal right shift
- Replace right shift with unsigned division unless there is a corresponding right shift

This partially reverts ede75ca8e64e9228782536a1c76f33ba116aa76f.

maple/more.c
@@ -525,7 +525,7 @@ more(
 
             lino++;
 
-            if ((lino % 32U == 0) && ((i = lino / 32U) < MAXBLOCK))
+            if ((lino & 31 == 0) && ((i = lino >> 5) < MAXBLOCK))
                 block[i] = foff - fimage;
 
 
@@ -762,12 +762,12 @@ re_key:
                     while (more_line(buf))
                     {
                         totallino++;
-                        if ((totallino % 32U == 0) && ((i = totallino / 32U) < MAXBLOCK))
+                        if ((totallino & 31 == 0) && ((i = totallino >> 5) < MAXBLOCK))
                             block[i] = foff - fimage;
                     }
 
                     /* 先位移到上一個 block 的尾端 */
-                    i = BMIN((totallino - b_lines) / 32U, MAXBLOCK - 1);
+                    i = BMIN((totallino - b_lines) >> 5, MAXBLOCK - 1);
                     foff = fimage + block[i];
                     i = i * 32;
 
@@ -801,7 +801,7 @@ re_key:
                 */
 
                 /* 先位移到上一個 block 的尾端 */
-                i = BMIN((lino - b_lines) / 32U, MAXBLOCK - 1);
+                i = BMIN((lino - b_lines) >> 5, MAXBLOCK - 1);
                 foff = fimage + block[i];
                 i = i * 32;
 

include/config.h
@@ -524,9 +524,9 @@
 #define MOVIE_LINES     (11)            /* 動畫最多有 11 列 */
 
 #define MENU_NOMOVIE_POS(y, x)  (y < (MOVIE_LINES + 2))  /* Suppress movie display when menu starts at (x, y) */
-#define MENU_XPOS       (d_cols / 2U + 23)    /* 選單開始的 (y, x) 座標 */
+#define MENU_XPOS       ((d_cols >> 1) + 23)  /* 選單開始的 (y, x) 座標 */
 #define MENU_YPOS       13
-#define MENU_XPOS_REF   (D_COLS_REF / 2U + 23)  /* For `get[y|x]_ref()` & `move_ref()` */
+#define MENU_XPOS_REF   ((D_COLS_REF >> 1) + 23)  /* For `get[y|x]_ref()` & `move_ref()` */
 #define MENU_YPOS_REF   13
 #define MENU_LOAD       1
 #define MENU_DRAW       2

innbbsd/channel.c
@@ -375,7 +375,7 @@ channelreader(
     if (len < ReadSize + 3)
     {
         len += (used + ReadSize);
-        len += (len / 8U);
+        len += (len >> 3);
 
         in->data = data = (char *) realloc(data, len);
         len -= used;

lib/xsort.c
@@ -77,7 +77,7 @@ void xsort(void *a, size_t n, size_t es, int (*cmp) (const void *lhs, const void
         pn = (char *)a + (n - 1) * es;
         if (n > 40)
         {
-            d = (n / 8U) * es;
+            d = (n >> 3) * es;
             pl = med3(pl, pl + d, pl + d + d, cmp);
             pm = med3(pm - d, pm, pm + d, cmp);
             pn = med3(pn - 2 * d, pn - d, pn, cmp);

maple/acct.c
@@ -268,7 +268,7 @@ void x_file(int mode,            /* M_XFILES / M_UFILES */
     if (mode == M_UFILES)
         domenu(list, MENU_YPOS_REF, MENU_XPOS_REF, 0, 0, 1);
     else
-        domenu(list, (B_LINES_REF-19)/2U, 0, 20, T_COLS_REF/2U, 2);
+        domenu(list, ((B_LINES_REF-19) >> 1), 0, 20, T_COLS_REF >> 1, 2);
 
     for (MENU *mptr = list; mptr->item.func; mptr++)
         free((char *)mptr->desc);
@@ -347,7 +347,7 @@ unsigned int bitset(unsigned int pbits, int count,    /* 共有幾個選項 */
             else
                 pbits ^= j;
         }
-        move(5 + (i % 16U), (i < 16 ? 0 : (b_cols+1) / 2U));
+        move(5 + (i % 16U), (i < 16 ? 0 : (b_cols+1) >> 1));
         if (perms[i])
             prints("%c %s %s", radix32[i], msg, perms[i]);
         else
@@ -378,7 +378,7 @@ unsigned int bitset(unsigned int pbits, int count,    /* 共有幾個選項 */
             }
 
             pbits ^= j;
-            move(5 + (i % 16U), (i < 16 ? 0 : (b_cols+1) / 2U) + 2);
+            move(5 + (i % 16U), (i < 16 ? 0 : (b_cols+1) >> 1) + 2);
             outs(msg);
         }
     }

maple/bbsd.c
@@ -859,7 +859,7 @@ tn_login(void)
                 level &= ~PERM_CHAT;
 
 /*
-            if ((cuser.numemail / 16U) > (cuser.numlogins + cuser.numposts))
+            if ((cuser.numemail >> 4) > (cuser.numlogins + cuser.numposts))
                 level |= PERM_DENYMAIL;*/
         }
 

maple/board.c
@@ -38,7 +38,7 @@ brh_alloc(
     if (size > brh_size)
     {
         /* size = (size & -BRH_PAGE) + BRH_PAGE; */
-        size += n / 16U;        /* 多預約一些記憶體 */
+        size += n >> 4;         /* 多預約一些記憶體 */
         base = (int *) realloc((char *) base, size);
 
         if (base == NULL)

maple/cache.c
@@ -562,7 +562,7 @@ observeshm_find(
     {
         for (count = oshm->total; count > 0;)
         {
-            datum = cache[mid = count / 2U];
+            datum = cache[mid = count >> 1];
             if (userno == datum)
                 return 1;
             if (userno > datum)
@@ -761,7 +761,7 @@ out_rle(
     else
         getyx(&y, &SINKVAL(int));
 
-    move(y, d_cols / 2U /*item_length[count++]*/);
+    move(y, d_cols >> 1/*item_length[count++]*/);
     while ((cc = (unsigned char) *str))
     {
         str++;
@@ -780,7 +780,7 @@ out_rle(
                         outs("\x1b[m");
                         clrtoeol();
                     }
-                    move(++y, d_cols / 2U /*item_length[count++]*/);
+                    move(++y, d_cols >> 1/*item_length[count++]*/);
                 }
                 else
                     outc(cc);
@@ -811,7 +811,7 @@ out_rle(
                 outs("\x1b[m");
                 clrtoeol();
             }
-            move(++y, d_cols / 2U /*item_length[count++]*/);
+            move(++y, d_cols >> 1/*item_length[count++]*/);
 
         }
         else

commit cbcc6bb0110674bc09874bc274d11370d838ed76

fix(M3 more): fix line position cache `block` broken since 39902b515d8fa67d8b39e62e7ff1dd991b3b883d
To prevent similar errors in the future, it is necessary to remember that
`&` && `|` have similar precedence as `&&` & `||` in C/C++.

This partially reverts 39902b515d8fa67d8b39e62e7ff1dd991b3b883d & ede75ca8e64e9228782536a1c76f33ba116aa76f.

maple/more.c
@@ -521,7 +521,7 @@ more(
 
             lino++;
 
-            if ((lino & 31 == 0) && ((i = lino >> 5) < MAXBLOCK))
+            if ((lino % 32 == 0) && ((i = lino >> 5) < MAXBLOCK))
                 block[i] = foff - fimage;
 
 
@@ -758,7 +758,7 @@ re_key:
                     while (more_line(buf))
                     {
                         totallino++;
-                        if ((totallino & 31 == 0) && ((i = totallino >> 5) < MAXBLOCK))
+                        if ((totallino % 32 == 0) && ((i = totallino >> 5) < MAXBLOCK))
                             block[i] = foff - fimage;
                     }
 